67284509c27ab8398c2cb442a699422a0a27a696
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-12-01 21:24:17 +0000

    Comments

--- js/blocks.js
+++ js/blocks.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function($) {
 
   indiciaData.onloadFns.push(function() {
+    // Copy grid attribute captions (from local config) into the grid
+    // headings which are initially populated from the database.
     if (indiciaData.speciesGrid && indiciaData.occurrenceAttributeCaptions) {
       $.each(indiciaData.speciesGrid, function(gridId) {
         $.each(indiciaData.occurrenceAttributeCaptions, function(attrId) {
