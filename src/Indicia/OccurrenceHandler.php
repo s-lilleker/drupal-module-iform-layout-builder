f57ccf5d91d2c167d35c086892c62d114a7001b2
John van Breda <john.vanbreda@biodiverseit.co.uk>
2021-05-18 14:53:07 +0100

    Fix zero abundance handling
    See https://github.com/simonrolph/NatureCounts/issues/61

--- src/Indicia/OccurrenceHandler.php
+++ src/Indicia/OccurrenceHandler.php
@@ -13,27 +13,26 @@ class OccurrenceHandler extends IndiciaRestClient {
    *   Drupal entity defining what type of form it is.
    *
    * @todo Uses data services so convert to REST API.
-   * @todo Behaviour around zero abundance data.
    */
   public function postRecord($data, $entity) {
     $config = \Drupal::config('iform.settings');
-    // @todo behaviour around zero abundance
     $auth = \data_entry_helper::get_read_write_auth($config->get('website_id'), $config->get('password'));
     $values = array_merge($data, [
       'website_id' => $config->get('website_id'),
       'survey_id' => $entity->field_survey_id->value,
       'sample:input_form' => trim(\Drupal::service('path.alias_manager')
-        ->getAliasByPath('/node/' . $entity->id()), '/')
+        ->getAliasByPath('/node/' . $entity->id()), '/'),
     ]);
     // Link to recording group/activity.
     if (!empty($_GET['group_id'])) {
       $values['sample:group_id'] = $_GET['group_id'];
     }
+    $zeroAttrs = $this->getAbundanceAttrs($auth['read'], $entity->field_survey_id->value);
     if ($entity->field_form_type->value === 'single') {
-      $submission = \data_entry_helper::build_sample_occurrence_submission($values);
+      $submission = \data_entry_helper::build_sample_occurrence_submission($values, $zeroAttrs);
     }
     else {
-      $submission = \data_entry_helper::build_sample_occurrences_list_submission($values);
+      $submission = \data_entry_helper::build_sample_occurrences_list_submission($values, FALSE, $zeroAttrs);
     }
     $response = \data_entry_helper::forward_post_to('save', $submission, $auth['write_tokens']);
     if (is_array($response) && array_key_exists('success', $response)) {
@@ -45,6 +44,47 @@ class OccurrenceHandler extends IndiciaRestClient {
         // @todo Display errors correctly alongside controls.
       }
     }
+  }
+
+  /**
+   * Finds the attributes for occurrence abundance.
+   *
+   * Used for zero abundance processing. Attribute term lists are also loaded.
+   *
+   * @param array $readAuth
+   *   Read authorisation tokens.
+   * @param int $surveyId
+   *   Survey ID.
+   *
+   * @return array
+   *   List of attribute definitions loaded from DB, keyed by attribute ID.
+   */
+  private function getAbundanceAttrs($readAuth, $surveyId) {
+    $attrOpts = [
+      'valuetable' => 'occurrence_attribute_value',
+      'attrtable' => 'occurrence_attribute',
+      'key' => 'occurrence_id',
+      'fieldprefix' => 'occAttr',
+      'extraParams' => $readAuth,
+      'survey_id' => $surveyId,
+    ];
+    $attrs = \data_entry_helper::getAttributes($attrOpts, FALSE);
+    $abundanceAttrs = [];
+    foreach ($attrs as &$attr) {
+      if ($attr['system_function'] === 'sex_stage_count') {
+        // If we have any lookups, we need to load the terms so we can check
+        // the data properly for zero abundance as term Ids are never zero.
+        if ($attr['data_type'] === 'L') {
+          $attr['terms'] = \data_entry_helper::get_population_data([
+            'table' => 'termlists_term',
+            'extraParams' => $readAuth + ['termlist_id' => $attr['termlist_id'], 'view' => 'cache', 'columns' => 'id,term'],
+            'cachePerUser' => FALSE,
+          ]);
+        }
+        $abundanceAttrs[$attr['attributeId']] = $attr;
+      }
+    }
+    return $abundanceAttrs;
   }
 
   /**
