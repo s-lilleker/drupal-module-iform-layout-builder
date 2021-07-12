7c018749ec5293f16a9d9e3c0f746207f7dd9313
John van Breda <john.vanbreda@biodiverseit.co.uk>
2021-07-12 14:45:05 +0100

    Delete button confirmation.

--- js/blocks.js
+++ js/blocks.js
@@ -12,4 +12,12 @@ jQuery(document).ready(function($) {
     }
   });
 
+  // Delete button confirmation.
+  $('.block-data-entry-submit-buttons-block button[value="DELETE"]').click(function(e) {
+    if (!confirm('Are you sure you want to delete all the data on this form?')) {
+      e.preventDefault();
+      return false;
+    }
+  });
+
 });
